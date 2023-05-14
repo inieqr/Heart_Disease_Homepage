7fb4b264ce6c9ca2e5e3b5f04de03bcbff86f8ea
inieqr <70246193+inieqr@users.noreply.github.com>
2023-05-14 04:15:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ with col2:
 col1, col2 = st.columns(2)
 
 with col1:
-   st.write("<h1 style='text-align: center; color: red;'>What is heart disease?</h1>", unsafe_allow_html=True)
+   st.write("<h1 style='text-align: center; color: #d91002;'>What is heart disease?</h1>", unsafe_allow_html=True)
 with col2:
    st.write("<h4 style='text-align: left; color: #2a2b2a;'>Heart disease is a general term that means the heart is not working properly. Some forms of heart disease are present at birth, while others develop as we age. Many forms of heart disease can be prevented by living an active, healthy lifestyle.</h4>", unsafe_allow_html=True)
   
