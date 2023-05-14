4635eff7de7f3e75943f388021e9769442fab16e
inieqr <70246193+inieqr@users.noreply.github.com>
2023-05-14 03:24:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit.components.v1 import html
 
-st.markdown("<h1 style='text-align: center; color: black;'>Heart Disease Prediction</h1>", unsafe_allow_html=True)
+# st.markdown("<h1 style='text-align: center; color: black;'>Heart Disease Prediction</h1>", unsafe_allow_html=True)
 #st.title("Heart Disease Prediction")
 
 # # Set the page title
@@ -9,7 +9,7 @@ st.markdown("<h1 style='text-align: center; color: black;'>Heart Disease Predict
 #   st.title("COVID-19 Prediction App")
 
 # Add a picture
-#st.image("https://editor.analyticsvidhya.com/uploads/95051Cardiovascular-Disease.jpg")
+st.image("https://editor.analyticsvidhya.com/uploads/95051Cardiovascular-Disease.jpg")
 #st.image("https://cdn.analyticsvidhya.com/wp-content/uploads/2018/08/health.jpg")
 #st.image("https://www.lenmed.co.za/wp-content/uploads/How-do-you-know-if-you-are-having-a-heart-attack.png")
 
