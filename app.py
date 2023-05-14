41b4bf9ec9e4b5565387a0e3580a590e36ee38f0
inieqr <70246193+inieqr@users.noreply.github.com>
2023-05-14 02:45:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ from streamlit.components.v1 import html
 
 # Add a picture
 #st.image("https://editor.analyticsvidhya.com/uploads/95051Cardiovascular-Disease.jpg")
-st.image("https://cdn.analyticsvidhya.com/wp-content/uploads/2018/08/health.jpg")
+#st.image("https://cdn.analyticsvidhya.com/wp-content/uploads/2018/08/health.jpg")
+st.image("https://croi.ie/wp-content/uploads/2022/08/image-6-13.jpg")
+
 
 st.markdown("<h1 style='text-align: center; color: #474646;'>Heart Disease Prediction</h1>", unsafe_allow_html=True)
 
